9d7da25f83d90810a6486c58a9f2d7c803f2804c
Yashit Agrawal <yashitagrawal1409@gmail.com>
2026-03-04 09:55:24 +0530

    added the fully secure backend

--- src/utility/middleware/multer.middleware.js
+++ src/utility/middleware/multer.middleware.js
@@ -26,7 +26,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit files size
 });
 
 module.exports = { upload };
